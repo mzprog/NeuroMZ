1cb2a2754907c8be5f3a271ac0a95d7441a835ea
Mohamad Zbib <toffahtarec@gmail.com>
2017-12-06 08:17:40 +0200

    Update ann.h

--- ann.h
+++ ann.h
@@ -1,6 +1,9 @@
 #ifndef _ANN_H_
 #define _ANN_H_
 
+#include <stdlib.h>
+#include <math.h>
+
 #define uint16 unsigned short
 
 //define the neurals structure
@@ -23,6 +26,14 @@ struct layers{
 
 
 };
+//define the train set
+
+struct trainSet{
+
+        double * input;
+        double * output;
+        struct trainSet * next;
+};
 
 //data and struct for file saving and openning file
 struct fileHead{
@@ -34,15 +45,7 @@ struct fileHead{
 		
 };
 
-double *  temp=0;//we should allocate as the bigest layer neurons.
-
-
-//aditional global varaibles
-struct layers * layer=0;
-uint16 layers_count;
-unsigned long steps=0;
-double * output_val=0;
-double learnRate = 0.1;//we can change it later by the main function
+//functions
 int INIT_NETWORK(int *layer_number,int layer_size);//preparing the neural network
 
 double * forward(double * data);//entring an array of double and the result is the array of the output
